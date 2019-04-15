9ce9f40ad226f39fd4d2f3bd6a1224ea2e7a0e6a
keke <ryehkk@yahoo.co.jp>
2019-04-16 08:01:09 +0900

    #41

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe AccessTokensController, type: :controller do
   describe '#create' do
     shared_examples_for "unauthorized_requests" do
-      let(:error) do
+      let(:authentication_error) do
         {
             'status' => '401',
             'source' => {"pointer" => '/code'},
@@ -19,7 +19,7 @@ RSpec.describe AccessTokensController, type: :controller do
 
       it 'should return proper error body' do
         subject
-        expect(json['errors']).to include(error)
+        expect(json['errors']).to include(authentication_error)
       end
     end
 
@@ -67,6 +67,30 @@ RSpec.describe AccessTokensController, type: :controller do
         user = User.find_by(login: 'jsmith1')
         expect(json_data['attributes']).to eq({'token' => user.access_token.token})
       end
+
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'when invalid request' do
+      let(:authentication_error) do
+        {
+            'status' => '403',
+            'source' => {"pointer" => '/headers/authorization'},
+            'title' => 'Not authorized',
+            'detail' => 'You have no right to access this resource'
+        }
+      end
+
+      subject {delete :destroy}
+
+      it 'should return 403 status code' do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'when valid request' do
 
     end
   end
