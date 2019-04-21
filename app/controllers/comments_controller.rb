9200535305f6be5edfe484ee235647bd175353dd
keke <dondohare55@gmail.com>
2019-04-22 07:47:50 +0900

    #51

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   skip_before_action :authorize!, only: :index
+  before_action :load_article, only: :create
   # GET /comments
   def index
     @comments = Comment.all
@@ -9,10 +10,10 @@ class CommentsController < ApplicationController
 
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @article.comments.build(comment_params.merge(user: current_user))
 
     if @comment.save
-      render json: @comment, status: :created, location: @comment
+      render json: @comment, status: :created, location: @article
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
@@ -20,8 +21,12 @@ class CommentsController < ApplicationController
 
   private
 
+  def load_article
+    @article = Article.find(params[:article_id])
+  end
+
   # Only allow a trusted parameter "white list" through.
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :article_id)
+    params.require(:comment).permit(:content, :article_id)
   end
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    content "MyText"
-    user nil
-    article nil
+    content "My Comment"
+    association :article
+    association :user
   end
 end
