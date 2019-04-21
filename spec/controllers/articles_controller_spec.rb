54f0063eac4c1b12cd43ed0da920eaf709319aa1
keke <dondohare55@gmail.com>
2019-04-21 12:04:08 +0900

    #48

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -144,7 +144,9 @@ describe ArticlesController do
   end
 
   describe '#update' do
-    let(:article) {create :article}
+    let(:user) {create :user}
+    let(:article) {create :article, user: user}
+    let(:access_token) {user.create_access_token}
 
     subject {patch :update, params: {id: article.id}}
 
@@ -154,6 +156,16 @@ describe ArticlesController do
 
     context 'when invalid code provided' do
       before {request.headers['authorization'] = 'Invalid token'}
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when trying to update not owned article' do
+      let(:other_user) {create :user}
+      let(:other_article) {create :article, user: other_user}
+
+      subject {patch :update, params: {id: other_article.id}}
+      before {request.headers['authorization'] = "Bearer #{access_token.token}"}
+
       it_behaves_like 'forbidden_requests'
     end
 
